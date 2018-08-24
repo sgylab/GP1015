1710ec2208fcfe8841e9700f46e68a7cdb5f08f7
jimmy60504 <jimmy60504@gmail.com>
2018-08-24 15:57:30 +0800

    Rewrite Caesar cipher

---
+++ 1061/final/Vigenere_cipher.py
@@ -0,0 +1,68 @@
+def loop_char(num, start_char, end_char):
+    loop_length = ord(end_char) - ord(start_char) + 1
+    if num < ord(start_char):
+        num += loop_length
+    elif num > ord(end_char):
+        num -= loop_length
+    return num
+
+
+def caesar(char, key):
+    num = ord(char)
+    if ord("A") <= num <= ord("Z"):
+        num += key
+        num = loop_char(num, "A", "Z")
+        new_char = chr(num)
+    elif ord("a") <= num <= ord("z"):
+        num += key
+        num = loop_char(num, "a", "z")
+        new_char = chr(num)
+    else:
+        new_char = char
+    return new_char
+
+
+def encrypt(text, keyword):
+    translated_text = ""
+    keyword = keyword.upper()
+    j = 0
+    for i in range(len(text)):
+        if text[i].isalpha():
+            key = ord(keyword[j % len(keyword)])
+            key -= ord("A")
+            translated_text += caesar(text[i], key)
+            j += 1
+        else:
+            translated_text += text[i]
+    return translated_text
+
+
+def decrypt(text, keyword):
+    translated_text = ""
+    keyword = keyword.upper()
+    j = 0
+    for i in range(len(text)):
+        if text[i].isalpha():
+            key = ord(keyword[j % len(keyword)])
+            key -= ord("A")
+            reversed_key = key * -1
+            translated_text += caesar(text[i], reversed_key)
+            j += 1
+        else:
+            translated_text += text[i]
+    return translated_text
+
+
+original_text = "Roses are red, Violets are blue."
+keyword = "SUGAR"
+
+print("Original text:")
+print(original_text)
+
+translated_text = encrypt(original_text, keyword)
+print("Translated text, key = %s:" % keyword)
+print(translated_text)
+
+decoded_text = decrypt(translated_text, keyword)
+print("Decoded text, key = %s:" % keyword)
+print(decoded_text)
---
+++ 1062/final/Caesar_cipher.py
@@ -0,0 +1,47 @@
+def loop_char(num, start_char, end_char):
+    loop_length = ord(end_char) - ord(start_char) + 1
+    if num < ord(start_char):
+        num += loop_length
+    elif num > ord(end_char):
+        num -= loop_length
+    return num
+
+
+def caesar(char, key):
+    num = ord(char)
+    if ord("A") <= num <= ord("Z"):
+        num += key
+        num = loop_char(num, "A", "Z")
+        new_char = chr(num)
+    elif ord("a") <= num <= ord("z"):
+        num += key
+        num = loop_char(num, "a", "z")
+        new_char = chr(num)
+    else:
+        new_char = char
+    return new_char
+
+
+key_number = "20180109"
+plaintext = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+
+print(plaintext)
+
+translated = ""
+counter = 0
+for s in plaintext:
+    current_key = int(key_number[counter % len(key_number)])
+    counter += 1
+    translated += caesar(s, current_key)
+
+print(translated)
+
+decoded = ""
+counter = 0
+for s in translated:
+    current_key = int(key_number[counter % len(key_number)])
+    counter += 1
+    reversed_key = current_key * -1
+    decoded += caesar(s, reversed_key)
+
+print(decoded)
