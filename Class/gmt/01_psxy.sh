8ab9cfec850f214f0073b063c3c5ad4d54491bb3
jimmy60504 <jimmy60504@gmail.com>
2018-10-06 17:45:18 +0800

    gmt class 03 update

--- Class/gmt/01_psxy.sh
+++ Class/gmt/01_psxy.sh
@@ -1,1 +1,6 @@
-echo 0 0 | gmt psxy -R-100/100/-80/120 -JX6.0 -Sc1 -Ba20g10/a20g10 > 01_grid.ps
+out_ps=01_grid.ps
+
+gmt set PS_MEDIA A4
+
+echo 0 0 | gmt psxy -R-100/100/-80/120 -JX6.0 -Sc1 -Ba20g10/a20g10 > $out_ps
+gmt psconvert $out_ps -P -Tf
--- Class/gmt/02_map.sh
+++ Class/gmt/02_map.sh
@@ -1,1 +1,6 @@
-gmt pscoast -R119/123/21/26 -Jm4 -B1 -W1 -Df -Ia -P > 02_Taiwan.ps
+out_ps=02_Taiwan.ps
+
+gmt set PS_MEDIA A4
+
+gmt pscoast -R119/123/21/26 -Jm4 -B1 -W1 -Df -Ia -P > $out_ps
+gmt psconvert $out_ps -P -Tf
