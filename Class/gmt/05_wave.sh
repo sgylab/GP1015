da7cd945185a093a1dc8a9fad8b7d5e11393190c
jimmy60504 <jimmy60504@gmail.com>
2018-10-08 16:03:45 +0800

    gmt 05 update

---
+++ Class/gmt/05_wave.sh
@@ -0,0 +1,14 @@
+out_ps=05_wave.ps
+ 
+gmt set PS_MEDIA A4
+ 
+# start gmt session
+gmt psxy -R0/1/0/1 -JX1c -T -K -P > $out_ps
+ 
+gmt psxy wave.txt -R-5/120/-160000/180000 -Jx0.1/0.00001 -W1 -B20/50000 -Xc -Yc -O -K >> $out_ps
+ 
+# end gmt session
+gmt psxy -R -J -O -T >> $out_ps
+ 
+gmt psconvert $out_ps -P -Tf # ps to pdf
+gmt psconvert $out_ps -P -Tg # ps to png
